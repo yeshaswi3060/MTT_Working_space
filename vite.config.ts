4370bf29e513208b071553c9a6af1e8163d5251f
Yeshaswi <yeshaswi3@gmail.com>
2025-11-23 15:32:07 +0530

    re-staring from fresh

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-// Trigger restart
-export default defineConfig({
-  plugins: [react()],
-})
